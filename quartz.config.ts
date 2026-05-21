99ad5ce677e5f6249ebbbf508573e03d27911509
ExtremeKrish <help.jokem@gmail.com>
2026-05-21 15:06:09 +0530

    customise theme

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Libre Baskerville",
+        body: "Crimson Pro",
         code: "IBM Plex Mono",
       },
       colors: {
