9acc18aff2a837b90a64b6b7814277c510400414
ExtremeKrish <help.jokem@gmail.com>
2026-05-22 18:54:14 +0530

    revert to working state

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "My Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Libre Baskerville",
+        body: "Crimson Pro",
         code: "IBM Plex Mono",
       },
       colors: {
